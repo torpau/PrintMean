636187c0ed0016940f208f9c1ac2a81a2f8ce5f8
Torsten Pause <torsten.pause@student.kyh.se>
2020-09-10 11:32:42 +0200

    initial commit

---
+++ src/com/company/PrintMean.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class PrintMean {
+
+    public static void main(String[] args) {
+        System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1,1,3,3));
+        System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1,2,3,4));
+        System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1,-2,3,-4));
+        System.out.println("Mean of -1, -11, 24, 2: " + meanCalc(-1,-11,24,2));
+    }
+
+    public static String meanCalc(int a, int b, int c, int d){
+        double mean = (a + b + c + d) / 4.0;
+        String meanString;
+        meanString = "" + mean + "";
+
+        return meanString;
+    }
+}
